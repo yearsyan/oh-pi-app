d8f17e1c5ce5e56c70c081fa4a71525c4a6b3a20
yearsyan <yearsyan@hotmail.com>
2026-08-05 00:02:24 +0800

    chore: bump version to 1.13

--- app/androidApp/build.gradle.kts
+++ app/androidApp/build.gradle.kts
@@ -59,8 +59,8 @@ android {
         applicationId = "io.github.yearsyan.pi"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 13
-        versionName = "1.12"
+        versionCode = 14
+        versionName = "1.13"
 
         externalNativeBuild {
             cmake {
