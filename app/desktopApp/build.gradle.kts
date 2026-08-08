be5e7b038743c65c4e4e185ee737f4bfee380968
yearsyan <yearsyan@hotmail.com>
2026-08-08 22:15:57 +0800

    chore: release 2.1.1

--- app/desktopApp/build.gradle.kts
+++ app/desktopApp/build.gradle.kts
@@ -105,7 +105,7 @@ compose.desktop {
         nativeDistributions {
             targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
             packageName = "io.github.yearsyan.ohpi"
-            packageVersion = "2.1.0"
+            packageVersion = "2.1.1"
             vendor = "Oh Pi App"
         }
     }
