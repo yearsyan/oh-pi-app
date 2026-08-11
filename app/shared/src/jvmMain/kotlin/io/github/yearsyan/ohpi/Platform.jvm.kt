2e62419d586ce12515411298d807d9021ff83f66
yearsyan <yearsyan@hotmail.com>
2026-08-11 22:30:07 +0800

    chore: release 2.2.8

--- app/shared/src/jvmMain/kotlin/io/github/yearsyan/ohpi/Platform.jvm.kt
+++ app/shared/src/jvmMain/kotlin/io/github/yearsyan/ohpi/Platform.jvm.kt
@@ -7,7 +7,7 @@ class JVMPlatform: Platform {
 
 actual fun getPlatform(): Platform = JVMPlatform()
 
-private const val DEFAULT_DESKTOP_APP_VERSION = "2.2.3"
+private const val DEFAULT_DESKTOP_APP_VERSION = "2.2.8"
 
 actual fun appVersion(): String =
     System.getProperty("ohpi.app.version")
